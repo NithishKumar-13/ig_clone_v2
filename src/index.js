6c0682e9e0d66a32a3c405984fc28b29396be240
Nithish Kumar <nithishpadma@gmail.com>
2021-09-17 10:00:21 +0530

    wrap the App component by AuthProvider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AuthProvider } from './context/AuthContext'
 import App from './App'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(
+    <AuthProvider>
+        <App />
+    </AuthProvider>, 
+document.querySelector('#root'))
