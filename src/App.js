c8a5f2cb3840c0c4f7307eaaf26b43cbe8162f8d
Nithish Kumar <nithishpadma@gmail.com>
2021-09-17 09:56:22 +0530

    import Login and Signup component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.scss'
+import Signup from './components/Signup/Signup'
+import Login from './components/Login/Login'
 
 const App = () => {
     return (
-        <div>
-            App
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/signup' component={Signup} />
+            </Switch>
+        </Router>
     )
 }
 
