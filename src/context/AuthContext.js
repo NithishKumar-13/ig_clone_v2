a4f419da4d8740da317aedbb7cb0b2c271d0c8a7
Nithish Kumar <nithishpadma@gmail.com>
2021-09-17 09:59:29 +0530

    create AuthContext.js

---
+++ src/context/AuthContext.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useReducer, createContext, useContext } from "react";
+
+const INITIAL_STATE = {
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  isLoading: false,
+  error: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN_START": {
+      return { ...state, isLoading: true, error: null, user: null };
+    }
+    case "LOGIN_SUCCESS": {
+      return { ...state, isLoading: false, error: null, user: action.payload };
+    }
+    default:
+      return state;
+  }
+};
+
+const AuthContext = createContext(INITIAL_STATE);
+
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
+
+  const value = {
+    user: state.user,
+    isLoading: state.isLoading,
+    error: state.error,
+    dispatch,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("consumer must be wrapped inside context provider");
+  }
+  return context;
+};
+
+export { AuthProvider, useAuth };
