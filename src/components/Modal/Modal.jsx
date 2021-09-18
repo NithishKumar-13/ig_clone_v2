23bf1e93e7a6b90b2e94bd43944955b51cacb11a
Nithish Kumar <nithishpadma@gmail.com>
2021-09-18 21:17:55 +0530

    finish Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,26 +3,65 @@ import ReactDOM from 'react-dom'
 import { useModal } from '../../context/ModalContext'
 import './Modal.scss'
 
-const Modal = () => {
-    const { isOpen } = useModal()
+const Modal = ({ isAdminOpened }) => {
+    const { isOpen, setIsOpen } = useModal()
 
     if(!isOpen) return null
 
     return ReactDOM.createPortal(
-        <div className='modal'>
-            <div className='modal__overlay' />
-            <div className='modal__body'>
-                <ul className='modal__lists'>
-                    <li className='modal__item'><button className='modal__button modal__button--main' tabIndex='0'>Unfollow</button></li>
-                    <li className='modal__item'><button className='modal__button' tabIndex='0'>Go to post</button></li>
-                    <li className='modal__item'><button className='modal__button' tabIndex='0'>Share to</button></li>
-                    <li className='modal__item'><button className='modal__button' tabIndex='0'>Copy link</button></li>
-                    <li className='modal__item'><button className='modal__button' tabIndex='0'>Cancel</button></li>
-                </ul>
-            </div>
-        </div>,
-        document.querySelector('#modal')
-    )
+      <div className="modal">
+        <div className="modal__overlay" onClick={() => setIsOpen(false)} />
+        <div className="modal__body">
+          <ul className="modal__lists">
+            {isAdminOpened ? (
+              <li className="modal__item">
+                <button
+                  className="modal__button modal__button--main"
+                  tabIndex="0"
+                >
+                  Delete
+                </button>
+              </li>
+            ) : (
+              <li className="modal__item">
+                <button
+                  className="modal__button modal__button--main"
+                  tabIndex="0"
+                >
+                  Unfollow
+                </button>
+              </li>
+            )}
+
+            <li className="modal__item">
+              <button className="modal__button" tabIndex="0">
+                Go to post
+              </button>
+            </li>
+            <li className="modal__item">
+              <button className="modal__button" tabIndex="0">
+                Share to
+              </button>
+            </li>
+            <li className="modal__item">
+              <button className="modal__button" tabIndex="0">
+                Copy link
+              </button>
+            </li>
+            <li className="modal__item">
+              <button
+                className="modal__button"
+                onClick={() => setIsOpen(false)}
+                tabIndex="0"
+              >
+                Cancel
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>,
+      document.querySelector("#modal")
+    );
 }
 
 export default Modal
