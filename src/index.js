8322faa98212a786885746eb8f9ac43553812719
Nithish Kumar <nithishpadma@gmail.com>
2021-09-18 17:46:08 +0530

    remove ViewPost provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { AuthProvider } from "./context/AuthContext";
-import { PostProvider } from "./context/PostContext";
 import App from "./App";
 
 ReactDOM.render(
   <AuthProvider>
-    <PostProvider>
       <App />
-    </PostProvider>
   </AuthProvider>,
   document.querySelector("#root")
 );
