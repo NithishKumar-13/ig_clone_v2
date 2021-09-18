59142c1afbd4fbb48a571cd504958bb7f758a003
Nithish Kumar <nithishpadma@gmail.com>
2021-09-18 21:16:53 +0530

    import Modal in index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { ModalProvider } from "./context/ModalContext";
+import Modal from "./components/Modal/Modal";
 import App from "./App";
 
 ReactDOM.render(
   <AuthProvider>
     <ModalProvider>
       <App />
+      <Modal />
     </ModalProvider>
   </AuthProvider>,
   document.querySelector("#root")
