cd85455e4130bf1199582831b5d7ead70c745339
Nithish Kumar <nithishpadma@gmail.com>
2021-09-18 17:40:33 +0530

    import Post provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AuthProvider } from './context/AuthContext'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { PostProvider } from "./context/PostContext";
+import App from "./App";
 
 ReactDOM.render(
-    <AuthProvider>
-        <App />
-    </AuthProvider>, 
-document.querySelector('#root'))
+  <AuthProvider>
+    <PostProvider>
+      <App />
+    </PostProvider>
+  </AuthProvider>,
+  document.querySelector("#root")
+);
