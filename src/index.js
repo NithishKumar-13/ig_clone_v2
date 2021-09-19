3e89fe7dc228e2d4b3a565847db5f81e79bd2e0b
Nithish Kumar <nithishpadma@gmail.com>
2021-09-19 11:47:00 +0530

    move Modal to ViewPost component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { ModalProvider } from "./context/ModalContext";
-import Modal from "./components/Modal/Modal";
 import App from "./App";
 
 ReactDOM.render(
   <AuthProvider>
     <ModalProvider>
       <App />
-      <Modal />
     </ModalProvider>
   </AuthProvider>,
   document.querySelector("#root")
