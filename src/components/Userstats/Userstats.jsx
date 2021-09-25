476f66e63321927a9e8deffcb44a6d427541bc13
Nithish Kumar <nithishpadma@gmail.com>
2021-09-25 21:26:38 +0530

    add follow and unfollow user functionality

--- src/components/Userstats/Userstats.jsx
+++ src/components/Userstats/Userstats.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import axios from 'axios'
+import { useAuth } from "../../context/AuthContext";
 import "./Userstats.scss";
 
-const Userstats = ({ username, img, totalPosts, followers, followings, fullName, isAdmin, isFollowing }) => {
-  
+const Userstats = ({ username, img, totalPosts, followers, followings, fullName, isAdmin, userId, isFollowing }) => {
+  const { user } = useAuth()
+ 
+  const handleFollowUser = async(receiver_username, receiver_fullName, receiver_id) => {
+      await axios.post(`http://localhost:8080/request/follow`, { sender_username: user.username, sender_fullName: user.full_name, sender_id: user.id, receiver_username, receiver_fullName, receiver_id })
+  }
+
+  const handleUnfollowUser = async(receiver_username, receiver_fullName, receiver_id) => {
+    await axios.post(`http://localhost:8080/request/unfollow`, { sender_username: user.username, sender_fullName: user.full_name, sender_id: user.id, receiver_username, receiver_fullName, receiver_id })
+  }
+
   return (
     <div className="user-stats">
       <div className="user-stats__left">
@@ -20,11 +31,11 @@ const Userstats = ({ username, img, totalPosts, followers, followings, fullName,
               Edit Profile
             </button>
           ) : isFollowing ? (
-            <button className="user-stats__btn user-stats__btn--unfollow">
+            <button className="user-stats__btn user-stats__btn--unfollow" onClick={() => handleUnfollowUser(username,fullName,userId)}>
               Unfollow
             </button>
           ) : (
-            <button className="user-stats__btn user-stats__btn--follow">
+            <button className="user-stats__btn user-stats__btn--follow" onClick={() => handleFollowUser(username,fullName, userId)}>
               Follow
             </button>
           )}
