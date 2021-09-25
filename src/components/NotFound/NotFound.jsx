206693677fc53014dacdeaef0fa3c9e61f83bc2b
Nithish Kumar <nithishpadma@gmail.com>
2021-09-25 11:21:37 +0530

    create NotFound component

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NotFound.scss'
+
+const NotFound = () => {
+    return (
+        <div>
+            Not Found
+        </div>
+    )
+}
+
+export default NotFound
