3b7e03d26f816010158e965e8780356d28c91654
Nithish Kumar <nithishpadma@gmail.com>
2021-09-25 17:31:14 +0530

    create LoadingSpinner

---
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './LoadingScreen.scss'
+
+const LoadingSpinner = () => {
+    return (
+      <div class="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
+}
+
+export default LoadingSpinner
