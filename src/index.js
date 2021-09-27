0cbc5fac5cf7d89967274958be2c5d4b2a5e353f
Nithish Kumar <nithishpadma@gmail.com>
2021-09-27 12:52:19 +0530

    delete ModalContext

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { AuthProvider } from "./context/AuthContext";
-import { ModalProvider } from "./context/ModalContext";
 import App from "./App";
 
 ReactDOM.render(
   <AuthProvider>
-    <ModalProvider>
       <App />
-    </ModalProvider>
   </AuthProvider>,
   document.querySelector("#root")
 );
